be18d40d4b6fe8ab302f06ccbdd5b4a2fbf53592
dd64 <dorianduranton64@gmail.com>
2023-10-19 22:45:08 +0200

    Update edt.py
    add optional argument and the day

--- bot/game/edt.py
+++ bot/game/edt.py
@@ -1,22 +1,67 @@
 
-import discord, requests, datetime, json
+import discord, requests, datetime, json, locale, calendar
 from discord import app_commands
 from __main__ import tree
 
-
+locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
 promos = ["E3a","E4a-A","E4a-B"]
 color = {"COU":0xF09300, "TD":0x00ABC0, "TP":0x378D3B}
 with open('asset/apiKey/eseoID.json', 'r') as file:
     ID = json.loads(file.read())
+    file.close()
+with open('asset/apiKey/admin.json', 'r') as file:
+    admin = json.loads(file.read())
+    file.close()
+
+def get_day_difference(target_day):
+    days = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"]
+    today = datetime.datetime.now().weekday()  # 0 = lundi, 1 = mardi, ..., 6 = dimanche
+    target_day_index = days.index(target_day.lower())
+
+    # Calculer la différence entre aujourd'hui et le jour cible
+    difference = target_day_index - today
+
+    # Ajuster la différence si le jour cible est dans la semaine précédente
+    if difference > 3:
+        difference -= 7
+    elif difference < -3:
+        difference += 7
+
+    return difference
+
 
 @tree.command(name="edt")
-@app_commands.describe(arg = "Name of your promotion ==> E3a, E4a-A, E4a-B")
-async def edt(interaction: discord.Interaction, arg: str):
-    if (arg not in promos):
+@app_commands.describe(name = "Name of your promotion")
+@app_commands.choices(name=[
+    app_commands.Choice(name="E3a", value="E3a"),
+    app_commands.Choice(name="E4a-A", value="E4a-A"),
+    app_commands.Choice(name="E4a-B", value="E4a-B")
+])
+@app_commands.describe(jour = "day of the week")
+@app_commands.choices(jour=[
+    app_commands.Choice(name="lundi", value="lundi"),
+    app_commands.Choice(name="mardi", value="mardi"),
+    app_commands.Choice(name="mercredi", value="mercredi"),
+    app_commands.Choice(name="jeudi", value="jeudi"),
+    app_commands.Choice(name="vendredi", value="vendredi"),
+    app_commands.Choice(name="samedi", value="samedi"),
+    app_commands.Choice(name="dimanche", value="dimanche")
+])
+async def edt(interaction: discord.Interaction, name: str, jour: str = datetime.datetime.now().strftime('%A')):
+    difference = get_day_difference(jour)
+
+    if (interaction.user.id == admin["ID"]):
+        await interaction.response.send_message(f"Une bonne journée a l'eseo pour la {name} pour {jour}:")
+    elif (name not in promos):
         await interaction.response.send_message("Mauvaise promo")
         return
-    await interaction.response.send_message(f"Une bonne journée a l'eseo pour la {arg} : ")
-    calendar = getCalendar(arg) #JSON
+    else:
+        await interaction.response.send_message(f"Une bonne journée a l'eseo pour la {name} pour {jour}: ")
+    calendar = getCalendar(name, jour) #JSON
+    if (calendar == []):
+        embed = discord.Embed(title="Pas de cours", description=":tada::tada::tada:", color=0x0000FF)
+        await interaction.channel.send(embed=embed) 
+        return
     for classs in calendar:
         embed = discord.Embed(title=classs["Libelle"].split("-")[0], description=classs["Emplacement"], color=color[classs["Code"]])
         embed.add_field(name="Prof", value=classs["Professeur"], inline=True)
@@ -29,17 +74,24 @@ async def edt(interaction: discord.Interaction, arg: str):
         await interaction.channel.send(embed=embed) 
     return
 
-def getCalendar(promo):
+def getCalendar(promo, jour):
     url = ID["URL"]
-    day = getDay() #"20231021T210000"
+    day = getDay(jour) #"20231021T210000"
     response = requests.get(f"https://reverse-proxy.eseo.fr/API-SP/API/agenda/user/{day[0]}/{day[1]}/{ID[promo]}")
     myResponse = json.loads(response.text)
     return myResponse
 
-def getDay():
-    day = str(datetime.datetime.today())
-    day = day.split(" ")[0].replace("-", "")
-    return(day+"T060000", day+"T210000")
+def getDay(jour):
+    diff = get_day_difference(jour)
+    today = datetime.datetime.now()
+    if today.day + diff < 0:
+        today = datetime.datetime(today.year, today.month - 1, today.day + diff)
+    elif today.day + diff > calendar.monthrange(today.year, today.month)[1]:
+        today = datetime.datetime(today.year, today.month + 1, today.day + diff)
+    else:
+        today = datetime.datetime(today.year, today.month, today.day + diff)
+    today = today.strftime('%Y%m%d')
+    return(today+"T060000", today+"T210000")
 
 
 
